7f2db0d6a8e242d31f3330ae6b778861ec2c8de4
James-Do9 <dojames3@yahoo.com>
2021-01-18 18:06:24 +0000

    Update

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Todo
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,10 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
+@app.route('/todo/dojam', methods=['GET'])
+def get_todo():
+    task_query = Todo.query.all()
+    return jsonify(task_query), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,34 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
-class User(db.Model):
+# pipenv run migrate create database migrations (if models.py is edited), only first 2 are needed
+# pipenv run upgrade run database migrations (if pending)
+# pipenv run start start flask web server (if not running)
+# pipenv run deploy deploy to heroku (if needed)
+class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    # email = db.Column(db.String(120), unique=True, nullable=False)
+    # password = db.Column(db.String(80), unique=False, nullable=False)
+    # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(80), unique=False, nullable=False)
+    label = db.Column(db.String(80), unique=False, nullable=False)
+    done = db.Column(db.String(80), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Todo %r>' % self.label
 
+    # def serialize(self):
+    #     return {
+    #         "id": self.id,
+    #         "email": self.email,
+    #         # do not serialize the password, its a security breach
+    #     }
+    
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
+            "label": self.label,
+            "done": self.done,
+            "username": self.username,
+            "id": self.id
             # do not serialize the password, its a security breach
         }
